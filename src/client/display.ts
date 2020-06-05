cefcc3d3ce3d2244e28816316b766f8b46a7633d
dkwasny <dkwasny67@gmail.com>
2020-06-05 19:48:53 -0400

    Various display.ts improvements

--- src/client/display.ts
+++ src/client/display.ts
@@ -1,23 +1,24 @@
 export class Display {
     private readonly canvas: HTMLCanvasElement;
     private readonly ctx: CanvasRenderingContext2D;
-    private readonly internalWidth: number;
-    private readonly internalHeight: number;
-    private readonly preserveAspectRatio: boolean;
+
+    internalWidth: number;
+    internalHeight: number;
+    preserveAspectRatio: boolean;
 
     constructor(pCanvas: HTMLCanvasElement, pInternalWidth: number, pInternalHeight: number, pPreserveAspectRatio: boolean) {
         this.canvas = pCanvas;
         this.ctx = this.canvas.getContext('2d') as CanvasRenderingContext2D;
-        this.internalHeight = pInternalHeight;
         this.internalWidth = pInternalWidth;
+        this.internalHeight = pInternalHeight;
         this.preserveAspectRatio = pPreserveAspectRatio;
     }
 
-    transposeX(value: number): number {
+    tX(value: number): number {
         return value * this.canvas.width / this.internalWidth;
     }
 
-    transposeY(value: number): number {
+    tY(value: number): number {
         return value * this.canvas.height / this.internalHeight;
     }
 
@@ -39,6 +40,10 @@ export class Display {
 
         this.canvas.height = actualHeight;
         this.canvas.width = actualWidth;
+    }
+
+    clear(): void {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 
     get context(): CanvasRenderingContext2D {
--- src/client/index.ts
+++ src/client/index.ts
@@ -1,16 +1,26 @@
 import { Display } from './display.js';
 
-const display = new Display(document.getElementById('kwasCanvas') as HTMLCanvasElement, 10, 10, true);
+const display = new Display(document.getElementById('kwasCanvas') as HTMLCanvasElement, 100, 100, true);
 
 const mainDiv = document.getElementById('mainDiv') as HTMLElement;
 const header = document.getElementById('header') as HTMLElement;
 
-const setRatio = function() {
+let currHeight = 0;
+let currWidth = 0;
+
+const setRes = function() {
     const input = prompt('New Internal Resolution (w:h)');
     if (input != null) {
         const [newW,newH] = input.split(':');
-        display.resize(+newW, +newH);
+        display.internalWidth = +newW;
+        display.internalHeight = +newH;
+        square.reset();
     }
+};
+
+const togglePreserveAspectRatio = function() {
+    display.preserveAspectRatio = !display.preserveAspectRatio;
+    display.resize(currWidth, currHeight);
 };
 
 const square = {
@@ -24,11 +34,15 @@ const square = {
         ctx.fillStyle = this.colorHex;
 
         ctx.fillRect(
-            display.transposeX(this.x),
-            display.transposeY(this.y),
-            display.transposeX(this.width),
-            display.transposeY(this.height)
+            display.tX(this.x),
+            display.tY(this.y),
+            display.tX(this.width),
+            display.tY(this.height)
         );
+    },
+    reset: function() {
+        this.x = 0;
+        this.y = 0;
     }
 };
 
@@ -48,21 +62,27 @@ const handleKeyPress = function(event: KeyboardEvent) {
         square.y += 1;
         break;
     case 'a':
-        setRatio();
+        setRes();
+        break;
+    case 'p':
+        togglePreserveAspectRatio();
         break;
     }
 };
 
-const draw = function() {
+const draw = function(timestamp: DOMHighResTimeStamp) {
     const windowHeight = mainDiv.clientHeight - header.clientHeight;
     const windowWidth = mainDiv.clientWidth;
 
-    display.resize(windowWidth, windowHeight);
-
-    display.context.clearRect(0, 0, display.transposeX(1920), display.transposeY(1080));
-
+    if (currHeight !== windowHeight || currWidth !== windowWidth) {
+        display.resize(windowWidth, windowHeight);
+        currHeight = windowHeight;
+        currWidth = windowWidth;
+    }
+    display.clear();
     square.draw(display);
+    window.requestAnimationFrame(draw);
 };
 
-window.setInterval(draw, 16);
+window.requestAnimationFrame(draw);
 window.addEventListener('keydown', handleKeyPress);
